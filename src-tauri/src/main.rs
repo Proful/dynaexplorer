d3cb2aced4c83a26b0707a68a8cc208f08f25e47
Proful <proful.sadangi@gmail.com>
2022-07-12 21:27:40 +1000

    Added list_tables

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,32 @@
     windows_subsystem = "windows"
 )]
 
-fn main() {
+use aws_sdk_dynamodb::{Client, Endpoint};
+
+#[tokio::main]
+async fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![list_tables])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn list_tables() -> Vec<String> {
+    let mut tables = Vec::new();
+    let shared_config = aws_config::from_env()
+        .endpoint_resolver(Endpoint::immutable(
+            "http://localhost:8000".parse().expect("valid URI"),
+        ))
+        .load()
+        .await;
+    let client = Client::new(&shared_config);
+    let req = client.list_tables().limit(10);
+    let resp = req.send().await.unwrap();
+    if let Some(table_names) = resp.table_names {
+        for table_name in table_names {
+            tables.push(table_name.to_string());
+        }
+    }
+    tables
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { invoke } from "@tauri-apps/api";
+import { SetStateAction, useEffect, useState } from "react";
+
 function App() {
+  const [tables, setTables] = useState<string[]>([]);
+
+  useEffect(() => {
+    invoke("list_tables").then((t) => setTables(t as SetStateAction<string[]>));
+  }, []);
+
   return (
-    <div className="flex h-screen text-red-500">
-      <header>Tables</header>
+    <div>
+      <h1>Tables</h1>
+      <ul>
+        {tables.map((t) => (
+          <li key={t}>
+            <a href="#">{t}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
