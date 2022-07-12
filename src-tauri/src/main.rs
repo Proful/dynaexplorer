3e32d17e56605d1c2633c2ca5cd7dee9c73e6974
Proful <proful.sadangi@gmail.com>
2022-07-12 22:50:32 +1000

    scan items

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,13 +2,15 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+// use tokio_stream::stream_ext::StreamExt;
 
 use aws_sdk_dynamodb::{Client, Endpoint};
+use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![list_tables])
+        .invoke_handler(tauri::generate_handler![list_tables, list_items])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -31,4 +33,49 @@ async fn list_tables() -> Vec<String> {
         }
     }
     tables
+}
+
+#[tauri::command]
+async fn list_items(table_name: String) -> Vec<String> {
+    let mut rows = Vec::new();
+    let shared_config = aws_config::from_env()
+        .endpoint_resolver(Endpoint::immutable(
+            "http://localhost:8000".parse().expect("valid URI"),
+        ))
+        .load()
+        .await;
+    let client = Client::new(&shared_config);
+    let items: Result<Vec<_>, _> = client
+        .scan()
+        .table_name(table_name)
+        .into_paginator()
+        .items()
+        .send()
+        .collect()
+        .await;
+    for item in items.unwrap() {
+        // println!("   {:?}", item);
+        // rows.push(item);
+        let mut records = Vec::new();
+        for (key, value) in item {
+            let v = if value.is_s() {
+                value.as_s().unwrap().to_string()
+            } else if value.is_ss() {
+                value.as_ss().unwrap().join(",").to_string()
+            } else if value.is_m() {
+                let mut m: String = "".to_string();
+                for (k, v) in value.as_m().unwrap() {
+                    m.push_str(&format!("{}:{}\n", k, v.as_s().unwrap()));
+                }
+                m
+            } else {
+                "".to_string()
+            };
+
+            // println!("{}:{}", key, value);
+            records.push(format!("{}::{}", key, v));
+        }
+        rows.push(records.join("~~"));
+    }
+    rows
 }
--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,33 @@ import { SetStateAction, useEffect, useState } from "react";
 
 function App() {
   const [tables, setTables] = useState<string[]>([]);
+  const [items, setItems] = useState<object[]>([]);
 
   useEffect(() => {
     invoke("list_tables").then((t) => setTables(t as SetStateAction<string[]>));
   }, []);
+
+  const listItems = function (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    tableName: string
+  ) {
+    e.preventDefault();
+    e.stopPropagation();
+    invoke("list_items", { tableName: tableName }).then((rows) => {
+      let items = [];
+      for (const row of rows as string[]) {
+        const kv_list = row.split("~~");
+        let row_data = {};
+        for (const kv of kv_list) {
+          const [k, v] = kv.split("::");
+          //@ts-ignore
+          row_data[k] = v;
+        }
+        items.push(row_data);
+      }
+      setItems(items);
+    });
+  };
 
   return (
     <div>
@@ -14,10 +37,37 @@ function App() {
       <ul>
         {tables.map((t) => (
           <li key={t}>
-            <a href="#">{t}</a>
+            <a href="#" onClick={(e) => listItems(e, t)}>
+              {t}
+            </a>
           </li>
         ))}
       </ul>
+      <h1>Items</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>PK</th>
+            <th>SK</th>
+            <th>title</th>
+            <th>tags</th>
+            <th>data</th>
+            <th>notes</th>
+            <th>shapes</th>
+          </tr>
+        </thead>
+        {items.map((item, i) => (
+          <tr key={i}>
+            <td>{item["PK"]}</td>
+            <td>{item["SK"]}</td>
+            <td>{item["title"]}</td>
+            <td>{item["tags"]}</td>
+            <td>{item["data"]}</td>
+            <td>{item["notes"]}</td>
+            <td>{item["shapes"]}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
